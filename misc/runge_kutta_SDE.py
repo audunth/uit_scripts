5234b2c1b81fe2d4f756c42b4ba9b75f1b9b12b0
Audun Theodorsen <ath019@uit.no>
2020-03-23 09:50:50 +0100

    Fixed recently introduced bug

--- misc/runge_kutta_SDE.py
+++ misc/runge_kutta_SDE.py
@@ -20,8 +20,7 @@ def SDE(T,x0=0,a = lambda x,i: 0,b = lambda x,i: 1,seed = None):
     X = np.zeros(len(T))
     X[0] = x0
     for i in range(len(T)-1):
-        x = X[i]
-        B = b(x,i)
-        B2 = x+B*(dt**(0.5)) - B
-        X[i+1] = x + a(x,i)*dt + B*dW[i] + 0.5*B2*(dW[i]**2-dt)*(dt**(-0.5))
-    return X
+        B = b(X[i],i)
+        B2 = b(X[i]+B*(dt**(0.5),i)) - B
+        X[i+1] = X[i] + a(X[i],i)*dt + B*dW[i] + 0.5*B2*(dW[i]**2-dt)*(dt**(-0.5))
+    return X, dW
