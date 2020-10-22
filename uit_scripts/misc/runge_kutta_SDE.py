dad562073fcd0545ebef316efec97b8a852a7c4a
audunth <audunth@users.noreply.github.com>
2020-10-22 13:56:47 +0200

    Update runge_kutta_SDE.py
    Added log option to SLE, added new SDE with gamma pdf, exp corrf.

--- uit_scripts/misc/runge_kutta_SDE.py
+++ uit_scripts/misc/runge_kutta_SDE.py
@@ -77,7 +77,7 @@ def SDE_GB(dt, N, x0=0.,mu=1., sigma=1., seed=None):
     return X
 
 @jit(nopython=True)
-def SDE_SLE(dt, N, x0=0., gamma=1., seed=None):
+def SDE_SLE(dt, N, x0=0., gamma=1., log=True, seed=None):
     """
     Fast calculation of the stochastic logistic equation with parameter gamma:
         dx_t = x_t(1-x_t/(1.+gamma)) dt + sqrt(2/(1+gamma)) x_t dW_t
@@ -85,19 +85,59 @@ def SDE_SLE(dt, N, x0=0., gamma=1., seed=None):
     dt: time step
     N: number of iterations
     x0: initial state
+    log: If True, estimate log(x_t) and then take the exponential.
     seed: seed for random state
     """
     if seed is not None:
         np.random.seed(seed)
     sqdt = dt**0.5
     dW = np.random.normal(0,sqdt,N-1)
-    dW2 = 0.5*(dW**2-dt)/sqdt
     sigma = np.sqrt(2/(1.+gamma))
-
     X = np.zeros(N)
-    X[0] = x0
-    for i in range(N-1):
-        B = sigma*X[i]
-        B2 = sigma*(X[i]+B*sqdt) - B
-        X[i+1] = X[i] + X[i]*(1.-X[i]/(1.+gamma))*dt + B*dW[i] + B2*dW2[i]
-    return X
+
+    if log:
+        X[0] = np.log(x0)
+        for i in range(N-1):
+            X[i+1] = X[i] + (gamma-np.exp(X[i]))*dt/(1.+gamma) + sigma*dW[i]
+        return np.exp(X)
+    else:
+        X[0] = x0
+        dW2 = 0.5*(dW**2-dt)/sqdt
+        for i in range(N-1):
+            B = sigma*X[i]
+            B2 = sigma*(X[i]+B*sqdt) - B
+            X[i+1] = X[i] + X[i]*(1.-X[i]/(1.+gamma))*dt + B*dW[i] + B2*dW2[i]
+        return X
+
+@jit(nopython=True)
+def SDE_GEXP(dt, N, x0=2., gamma=2., sqrt=True, seed=None):
+    """
+    Fast calculation of a gamma distributed process (shape parameter gamma, scale parameter 1) with exponential autocorrelation function:
+        dx_t = (gamma-x_t) dt + sqrt(2 x_t) dW_t
+    See SDE for the method.
+    OBS: This is numerically stable only for gamma > 1.5 or so
+        (for dt = 1e-3, the stability threshold may depend on dt)
+    dt: time step normalized by correlation time
+    N: number of iterations
+    x0: initial state
+    sqrt: If True, estimate sqrt(x_t) and then square it.
+    seed: seed for random state
+    """
+    if seed is not None:
+        np.random.seed(seed)
+    sqdt = dt**0.5
+    dW = np.random.normal(0,sqdt,N-1)
+    X = np.zeros(N)
+    if sqrt:
+        X[0] = np.sqrt(x0)
+        for i in range(N-1):
+            X[i+1] = X[i] + (0.5/X[i])*(gamma-0.5-X[i]**2)*dt + dW[i]/np.sqrt(2.)
+        return X**2
+    else:
+        X[0] = x0
+        dW2 = 0.5*(dW**2-dt)/sqdt
+        for i in range(N-1):
+            B = np.sqrt(2*X[i])
+            B2 = np.sqrt(2*(X[i]+B*sqdt)) - B
+            X[i+1] = X[i] + (gamma-X[i])*dt + B*dW[i] + B2*dW2[i]
+        return X
